a302035a2f92b0594c37d87163b7ebb94c139d29
Mario Patronelli <mario@raffles.com.ar>
2019-11-25 13:34:13 +0000

    Install Laravel Socialite. Add providers to config/services.php

--- config/services.php
+++ config/services.php
@@ -30,4 +30,33 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URI')
+    ],
+    
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
+    
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URI')
+    ],
+    
+    'linkedin' => [
+        'redirect' => env('LINKEDIN_REDIRECT_URI'),
+        'client_id' => env('LINKEDIN_KEY'),
+        'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
+    ],
+
+    'twitter' => [
+        'redirect' => env('TWITTER_REDIRECT_URI'),
+        'client_id' => env('TWITTER_KEY'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+    ],
 ];
